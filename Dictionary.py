d647a07ef6d057d03996520a192b35b0b89ec551
Cheryl50 <kimberlynewton216@gmail.com>
2025-12-19 18:10:16 +0100

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+Ethopia_dict=(
+   'Good Morning' : 'Tena yistilign'
+   'Please' : 'Ebakesh' 
+     'Thank You': 'Ameseginalehu' 
+     'Welcome':'metash'  
+    'Beautiful' : 'Konjo' 
+    'Friend' : 'Guadegna'
+    'yes' : 'Awo'
+    'No' : 'Ay'
+    'sorry' :'Yikirta'
+    'Food' : 'Migib'
+    'Water' : 'Wuhaa'
+    'love' : 'Fikir'
+    'God' : 'Egziabher'
+    'Peace' : 'Selam'
+    'Child' : 'Lij'
+    'School' : 'Timhirt bet'
+    'Work' : 'Sira'
+    'Day' : 'Ken'
+    'Time' : 'Gize'
+    'Road' : 'Menged'
+    )
+
+word = input("Enter a Ethopia word :")
+print(Ethopia_dict[word])
